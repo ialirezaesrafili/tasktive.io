e2d62a33e54b4074725f211d390624a5158e5c7c
ialirezaesrafili <alirezaesrafili53@gmail.com>
2025-02-09 21:57:41 +0330

    using db.js function apply it on application function according to db.js and using try , catch for error handling on it

--- app/app.js
+++ app/app.js
@@ -3,11 +3,21 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import envInstance from "../config/env.js";
+import dbConnection from "../config/db.js";
 
 
-function application() {
+async function application() {
     const app = express();
     const PORT = envInstance.port;
+    const mongoDB = envInstance.mongoDev;
+
+    try {
+        await dbConnection(mongoDB);
+    } catch (err) {
+        console.error(`[APP] Failed to connect to MongoDB: ${err.message}`);
+        process.exit(1);
+    }
+
     app.use(express.json());
     app.use(express.urlencoded({extended: false}));
     app.use(cors());
