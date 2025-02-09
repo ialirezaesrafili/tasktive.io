82e1e6031a2b74c2f5f49fac2b90ad7386732290
ialirezaesrafili <alirezaesrafili53@gmail.com>
2025-02-09 21:55:16 +0330

    apply env for production, developments, and backup

--- config/env.js
+++ config/env.js
@@ -3,14 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// create instance for environment
 
+// create instance for environment
 const envInstance = Object.freeze({
     port: process.env.PORT || 3000,
-    mongo: process.env.MONGODB || 'mongodb://localhost:27017',
+    mongoDev: process.env.MONGODBDEV,
+    mongoProduction: process.env.MONGOPRODUCTION,
+    mongoBackUp: process.env.MONOGOBACKUP,
     salt: parseInt(process.env.SALT) || 10,
     jwtSecret: process.env.JWT_SECRET,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV,
 })
 
 
